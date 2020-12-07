86a0c7d2019dee1d8226a9ad4b51126f7024770f
ahmed <a.bennaji96@gmail.com>
2020-12-07 16:31:53 -0700

    home page completed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,60 +1,56 @@
 <template>
-<v-content>
-
-  <div class="image">
-<v-img  src="@/assets/background.jpg"
- max-height="800"
-	max-width="1915"
-
-></v-img>
-  </div>
-<h1 class = "first line"> Hello,I am Ahmed Naji. </h1>
-<h1 class = "secondline"> I am a Computer Science Student </h1>
-<h1 class = "thirdline"> Welcome to my portfolio</h1>   
-</v-content>
+  <v-content>
+    <div class="image">
+      <v-img
+        src="@/assets/background.jpg"
+        max-height="800"
+        max-width="1915"
+      ></v-img>
+    </div>
+    <h1 class="first line">Hello,I am Ahmed Naji.</h1>
+    <h1 class="secondline">I am a Computer Science Student</h1>
+    <h1 class="thirdline">
+      Welcome to <v-btn depressed color="error"> my portfolio </v-btn>
+    </h1>
+  </v-content>
 </template>
 
 <script>
 // @ is an alias to /src
 
 export default {
-  name: "Home", 
-
+  name: "Home"
 };
 </script>
 
-
 <style lang="scss" scoped>
-
-.image { 
-   position: relative; 
-   width: 100%;
-   height:100% ;
-   
+.image {
+  position: relative;
+  width: 100%;
+  height: 100%;
 }
 
-h1{
+h1 {
   margin-top: 200px;
-  font-family:'Raleway', sans-serif;
+  font-family: "Raleway", sans-serif;
   font-weight: 300;
   font-size: 32px;
   margin-left: 90px;
-   position: absolute; 
-   top: 200px; 
-   left: 0; 
-   width: 100%; 
+  position: absolute;
+  top: 200px;
+  left: 0;
+  width: 100%;
 }
-.secondline{
-  position: absolute; 
-   top: 240px; 
-   left: 0; 
-   width: 100%; 
+.secondline {
+  position: absolute;
+  top: 240px;
+  left: 0;
+  width: 100%;
 }
-.thirdline{
-  position: absolute; 
-   top: 280px; 
-   left: 0; 
-   width: 100%; 
-
+.thirdline {
+  position: absolute;
+  top: 280px;
+  left: 0;
+  width: 100%;
 }
 </style>
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,18 +1,80 @@
 <template>
-<v-content>
-Portfolio
-</v-content>
+  <v-container>
+    <v-flex mt-10>
+      <v-layout>
+        <v-flex>
+          <template v-for="(app, i) in apps">
+            <v-card flat color="transparent" :key="i">
+              <v-card-title>
+                <v-layout>
+                  <v-flex mt-10>
+                    <v-card
+                      style="border-raduis:20px"
+                      color="blue"
+                      width="150px"
+                      height="150px"
+                    >
+                    </v-card>
+                  </v-flex>
+                </v-layout>
+              </v-card-title>
+              <v-card-text>{{ description }}</v-card-text>
+              <div class="title mt-2">technologies used</div>
+              <template v-for="(tech, i) in app.technologies">
+                <v-btn icon :key="i">
+                  <v-icon>{{ tech.icon }}</v-icon>
+                </v-btn>
+              </template>
+              <div class="title mt-2">Code</div>
+              <div>{{ app.code }}</div>
+            </v-card>
+          </template>
+        </v-flex>
+      </v-layout>
+    </v-flex>
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
 
 export default {
-  name: "Portfolio", 
+  name: "Portfolio",
+  components: {
+    //hellworld
+  },
 
+  data() {
+    return {
+      description: "Database systems projects",
+      apps: [
+        {
+          name: "Database System"
+        }
+      ],
+      technologies: [
+        {
+          name: "Spring Java",
+          icon: "mdi-java",
+          link: ""
+        },
+        {
+          name: "sqlite3",
+          icon: "fa-data",
+          link: ""
+        },
+        {
+          name: "redis",
+          icon: "",
+          link: ""
+        }
+      ],
+      code: {
+        name: "github"
+      }
+    };
+  }
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>

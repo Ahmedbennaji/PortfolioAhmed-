48d068c2696ddd357b90f47f788738646bd0d8fc
ahmed <a.bennaji96@gmail.com>
2020-12-14 18:53:30 -0700

    Edit Contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,41 +1,45 @@
 <template>
-  <v-container>
-    <h2 class="display-2 font-weight-bold mb-3 text-uppercase text-center">
-      Contact Me
-    </h2>
+  <v-sheet id="contact" color="#e3e1e1" dark tag="section" tile>
+    <div class="py-12"></div>
+    <v-container>
+      <h2 class="display-2 font-weight-bold mb-3 text-uppercase text-center">
+        Contact Me
+      </h2>
 
-    <v-responsive class="mx-auto mb-12" width="56">
-      <v-divider class="mb-1"></v-divider>
+      <v-responsive class="mx-auto mb-5" width="56">
+        <v-divider class="mb-1"></v-divider>
 
-      <v-divider></v-divider>
-    </v-responsive>
+        <v-divider></v-divider>
+      </v-responsive>
 
-    <v-theme-provider light>
-      <v-row>
-        <v-col cols="12">
-          <v-text-field flat label="Name*" solo></v-text-field>
-        </v-col>
+      <v-theme-provider light>
+        <v-row>
+          <v-col cols="12">
+            <v-text-field flat label="Name*" solo></v-text-field>
+          </v-col>
 
-        <v-col cols="12">
-          <v-text-field flat label="Email*" solo></v-text-field>
-        </v-col>
+          <v-col cols="12">
+            <v-text-field flat label="Email*" solo></v-text-field>
+          </v-col>
 
-        <v-col cols="12">
-          <v-text-field flat label="Subject*" solo></v-text-field>
-        </v-col>
+          <v-col cols="12">
+            <v-text-field flat label="Subject*" solo></v-text-field>
+          </v-col>
 
-        <v-col cols="12">
-          <v-textarea flat label="Message*" solo></v-textarea>
-        </v-col>
+          <v-col cols="12">
+            <v-textarea flat label="Message*" solo></v-textarea>
+          </v-col>
 
-        <v-col class="mx-auto" cols="auto">
-          <v-btn color="accent" x-large>
-            Submit
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-theme-provider>
-  </v-container>
+          <v-col class="mx-auto" cols="auto">
+            <v-btn color="accent" x-large>
+              Submit
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-theme-provider>
+    </v-container>
+    <div class="py-12"></div>
+  </v-sheet>
 </template>
 
 <script>
